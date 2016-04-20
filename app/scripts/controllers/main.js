e2477aef707a29c758c48deb83c70aa6fe786fa7
Fhad Sheikh <fhad@clockworks.ca>
2016-04-20 11:49:19 -0400

    Assign To

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,6 @@
  * Controller of the ximApp
  */
 angular.module('ximApp')
-.controller('CardsCtrl', function($scope,$pusher){
-    
-    
-})
-.controller('TechstatsCtrl', function($scope,$pusher){
-    
-    var client = new Pusher('2be55f84069c8f50ee29');
-    var pusher = $pusher(client);
-    
-    var tix = pusher.subscribe('tix');
-    
-   
-})
 .controller('FeedCtrl', function($scope,$pusher){
     var client = new Pusher('2be55f84069c8f50ee29');
     var pusher = $pusher(client);
@@ -33,10 +20,6 @@ angular.module('ximApp')
     });
     
     $scope.feed = []
-    
-})
-.controller('TicketsCtrl', function($scope,$pusher,$http){
-
     
 })
 .controller('MainCtrl', function ($scope,$pusher,$http) {
@@ -102,25 +85,32 @@ angular.module('ximApp')
         
     });
     
-    $scope.openTicket = function()
+    var backToList = function()
     {
-        $scope.singleTicket = true;
+        $scope.singleTicket = false;
+        $scope.assigning = false;
     }
     
     $scope.backToList = function()
     {
-        $scope.singleTicket = false;
+        backToList();
+        $scope.ticketDetails = null;
     }
     
     $scope.filterList = function(label)
     {
-        if(label == 'Open Tickets')
+        if($scope.assigning){
+            swal({   title: "Assign ticket to "+label+"?",   text: "",   type: "warning",   showCancelButton: true,   confirmButtonColor: "#5cc7b2",   confirmButtonText: "Yes, assign it!",   cancelButtonText: "No, cancel!",   closeOnConfirm: false,   closeOnCancel: false }, function(isConfirm){   if (isConfirm) {     swal("Assigned!", "Ticket has been assigned to "+label, "success");   } else {     swal("Cancelled", "Click another name to assign ticket to a tech.", "error");   } });
+        } else {
+            $scope.singleTicket = false;
+            if(label == 'Open Tickets')
             {
-                $scope.search = $scope.search.$;
+                $scope.search = '';
             } else {
-                
-            $scope.search = label;  
+                $scope.search = label;  
             }
+        }
+        
     }
     
     /* TECH STATS */
@@ -129,4 +119,42 @@ angular.module('ximApp')
         $scope.techs = data;
     });
     
-});
+    /* TICKET */
+    
+    $scope.openTicket = function(ticket)
+    {
+        
+        $scope.singleTicket = true;
+        
+        $scope.ticket = ticket;
+        
+        $http.get('http://localhost:8080/tsk/tickets/ticket?issueID='+ticket.issueId)
+        .success(function(res1){
+            $scope.ticketDetails = res1;
+        })
+        .error(function onError(err){
+            console.log(err);
+        });
+        
+        $http.get('http://localhost:8080/tsk/tickets/comments?issueID='+ticket.issueId)
+        .success(function(res){
+            $scope.comments = res;
+        })
+        .error(function(err){
+            console.log(err);
+        });
+        
+        
+    }
+    
+    $scope.assignTo = function(ticket)
+    {
+        if($scope.assigning){
+            $scope.assigning = false;
+        } else {
+            $scope.assigning = true;
+        }
+    }
+    
+    
+})
